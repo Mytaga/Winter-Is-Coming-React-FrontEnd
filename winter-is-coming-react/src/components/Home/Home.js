11c0a001432edf5cb7bad9a5c64a985503efe4ca
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2023-03-29 22:15:10 +0300

    Add files via upload

--- winter-is-coming-react/src/components/Home/Home.js
+++ winter-is-coming-react/src/components/Home/Home.js
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { AuthContext } from '../../contexts/AuthContext';
+import { useContext } from "react";
 
 const Home = () => {
+    const { isAuthenticated } = useContext(AuthContext);
     return (
         <div id="intro-example" className="p-5 text-center bg-image">
             <div className="mask">
@@ -8,12 +11,12 @@ const Home = () => {
                     <div className="text-white">
                         <h1 className="mb-3">Winter is coming so prepare yourself....</h1>
                         <h5 className="mb-4">Get the latest info about resorts around Europe</h5>
-                        <Link to={"/login"} className="btn btn-outline-light btn-lg m-2">
+                        {!isAuthenticated && (<Link to={"/login"} className="btn btn-outline-light btn-lg m-2">
                             Login to explore
-                        </Link>
-                        <Link to={"/register"} className="btn btn-outline-light btn-lg m-2">
+                        </Link>)}
+                        {!isAuthenticated && (<Link to={"/register"} className="btn btn-outline-light btn-lg m-2">
                             Sign up now
-                        </Link>
+                        </Link>)}
                     </div>
                 </div>
             </div>
@@ -21,4 +24,4 @@ const Home = () => {
     );
 }
 
-export default Home;
+export default Home; 
